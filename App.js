1fe186e36eafc3f22413e29d5cdf45c7744c1205
Harleen <43519990+HarleenKaur30@users.noreply.github.com>
2022-01-05 16:48:46 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-
-import React from "react";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { StatusBar } from "expo-status-bar";
+import LoginScreen from "./app/screens/LoginScreen";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <LoginScreen />;
 }
+
+ 
