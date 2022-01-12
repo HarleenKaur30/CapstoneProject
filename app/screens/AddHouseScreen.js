05b18e48e2f8fb94a69051801d5971b63a0aa004
gjsingh10 <gjsingh@ualberta.ca>
2022-01-11 18:48:11 -0700

    Create AddHouseScreen.js

---
+++ app/screens/AddHouseScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+
+function AddHouseScreen(props) {
+  return (
+    <SafeAreaView>
+      <Text>Add House</Text>
+    </SafeAreaView>
+  );
+}
+
+export default AddHouseScreen;
