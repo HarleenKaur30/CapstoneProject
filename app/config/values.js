ffa4f29e52fb9c350cdf3c4e2dc782e2bf797a1f
gjsingh10 <gjsingh@ualberta.ca>
2022-01-11 18:47:40 -0700

    Create values.js

---
+++ app/config/values.js
@@ -0,0 +1,16 @@
+export default {
+  maxNumberOfHouses: 5,
+  maxNumberOfBlinds: 10,
+  houses: {
+    number: 3,
+    house1: {
+      blinds: 7,
+    },
+    house2: {
+      blinds: 9,
+    },
+    house3: {
+      blinds: 10,
+    },
+  },
+};
