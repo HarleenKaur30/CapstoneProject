439a6f11a4b24f0ca06a84bc05ddbeba4814f717
gjsingh10 <gjsingh@ualberta.ca>
2022-01-11 18:47:25 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,17 +2,15 @@ import LoginScreen from "./app/screens/LoginScreen";
 import HomeScreen from "./app/screens/HomeScreen";
 import TestScreen from "./app/screens/TestScreen";
 import Tabs from "./app/screens/Tabs";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AddDeviceScreen from "./app/screens/AddDeviceScreen";
-import colors from './app/config/colors';
-
-
-
+import AddHouseScreen from "./app/screens/AddHouseScreen";
+import AddBlindsScreen from "./app/screens/AddBlindsScreen";
+import colors from "./app/config/colors";
 
 //This navigator is responsible for moving across different stack screens
 const Stack = createNativeStackNavigator();
-
 
 /* Note that in the code below, nested navaigation is being used.
  Tab Navigator defined on the Tabs.js is nested within the Stack Navigator.
@@ -21,12 +19,17 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator  initialRouteName= "Tabs" >
-        <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }}   />
+      <Stack.Navigator initialRouteName="Tabs">
+        <Stack.Screen
+          name="Tabs"
+          component={Tabs}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Add Device" component={AddDeviceScreen} />
         <Stack.Screen name="Test" component={TestScreen} />
-        
+        <Stack.Screen name="Add House" component={AddHouseScreen} />
+        <Stack.Screen name="Add Blinds" component={AddBlindsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-} 
+}
