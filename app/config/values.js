61016aa53aa364d4a7e19673d0d9421e858227d5
gjsingh10 <gjsingh@ualberta.ca>
2022-01-12 23:41:20 -0700

    Update values.js

--- app/config/values.js
+++ app/config/values.js
@@ -4,15 +4,18 @@ export default {
   houses: {
     number: 2,
     house1: {
+      name: "House",
       blinds: 7,
       blind1: {
         name: "Bedroom",
       },
     },
     house2: {
+      name: "Vacation House",
       blinds: 9,
     },
     house3: {
+      name: "Work",
       blinds: 10,
     },
   },
