4bd28504ec060eaa2085d42342aab01d5e85174f
gjsingh10 <gjsingh@ualberta.ca>
2022-01-12 23:41:24 -0700

    Update AddBlindsScreen.js

--- app/screens/AddBlindsScreen.js
+++ app/screens/AddBlindsScreen.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  Text,
   StyleSheet,
   NativeModules,
   SafeAreaView,
   Platform,
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
 } from "react-native";
-import Tabs from "./Tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto+Slab|Roboto:300,400,500,700"
+  rel="stylesheet"
+/>;
 
 import colors from "../config/colors";
+import values from "../config/values";
+import { Picker } from "@react-native-picker/picker";
 
 function AddBlindsScreen(props) {
+  const [country, setCountry] = useState("Unknown");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Add Blinds</Text>
+      <Picker
+        selectedValue={country}
+        onValueChange={(value, index) => setCountry(value)}
+        mode="dialogue" // Android only
+        style={styles.picker}
+      >
+        <Picker.Item label="Please select your country" value="Unknown" />
+        <Picker.Item label={values.houses.house1.name} value="Australia" />
+        <Picker.Item label="Belgium" value="Belgium" />
+        <Picker.Item label="Canada" value="Canada" />
+        <Picker.Item label="India" value="India" />
+        <Picker.Item label="Japan" value="Japan" />
+      </Picker>
+      <Text style={styles.text}>Your conuntry: {country}</Text>
     </SafeAreaView>
   );
 }
@@ -24,7 +49,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.white,
     //paddingTop: Platform.OS === "android" ? StatusBarManager.HEIGHT : 0,
-    padding: 20,
+    padding: "5%",
   },
 });
 
