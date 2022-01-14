70eec4e9a9aa5c10ec2c765a237ca22f1977b6a7
gjsingh10 <gjsingh@ualberta.ca>
2022-01-13 19:36:38 -0700

    Create ListHouse.js

---
+++ app/components/ListHouse.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, StyleSheet, Text, TouchableHighlight } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+
+function ListHouse({ houseName, numberBlinds, onPress }) {
+  return (
+    <TouchableHighlight onPress={onPress} underlayColor={colors.silvergray}>
+      <View style={styles.container}>
+        <MaterialIcons name="home" size={40} color={colors.orange} />
+        <View style={styles.textContainer}>
+          <Text style={styles.houseNameText}>{houseName}</Text>
+          <Text style={styles.blindsText}>
+            Number of Blinds: {numberBlinds}/10
+          </Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+}
+
+const styles = StyleSheet.create({
+  blindsText: {
+    color: colors.medium,
+    fontSize: 13,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    fontStyle: "italic",
+  },
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    padding: "5%",
+    backgroundColor: colors.white,
+  },
+  houseNameText: {
+    color: colors.dark,
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    fontWeight: "bold",
+  },
+  textContainer: {
+    paddingLeft: "5%",
+  },
+});
+
+export default ListHouse;
