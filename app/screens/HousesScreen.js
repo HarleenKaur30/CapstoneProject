569ec28e89a44e6537a8041e15944a92a33bd314
gjsingh10 <gjsingh@ualberta.ca>
2022-01-13 21:03:09 -0700

    Update HousesScreen.js

--- app/screens/HousesScreen.js
+++ app/screens/HousesScreen.js
@@ -1,12 +1,38 @@
 import React from "react";
-import { Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, FlatList, View, Alert } from "react-native";
 
-function HousesScreen(props) {
+import ListHouse from "../components/ListHouse";
+import ListItemSeperator from "../components/ListItemSeperator";
+import colors from "../config/colors";
+import values from "../config/values";
+import houses from "../config/houses";
+
+function HousesScreen({ navigation }) {
   return (
-    <SafeAreaView>
-      <Text>Houses</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <FlatList
+        data={houses}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListHouse
+            houseName={item.name}
+            numberBlinds={item.numberBlinds}
+            onPress={() =>
+              navigation.navigate("Find Blinds", { ...item.title })
+            }
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeperator}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+});
 
 export default HousesScreen;
