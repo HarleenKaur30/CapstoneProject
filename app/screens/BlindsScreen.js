c564fb8d5c8ce01dc7f673ba55608fde0e58b28c
gjsingh10 <gjsingh@ualberta.ca>
2022-01-14 00:58:09 -0700

    Create BlindsScreen.js

---
+++ app/screens/BlindsScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View, SafeAreaView, Button, Platform } from "react-native";
+function BlindsScreen({ navigation }) {
+  return (
+    <SafeAreaView>
+      <Text> Blinds Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default BlindsScreen;
