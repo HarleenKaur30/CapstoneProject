960e065ede9aa7e191ebc7c4451a129fdc9d0f43
gjsingh10 <gjsingh@ualberta.ca>
2022-01-14 14:04:55 -0700

    Create BlindsInformationScreen.js

---
+++ app/screens/BlindsInformationScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, SafeAreaView, Button, Platform } from "react-native";
+
+function BlindsInformationScreen({ navigation }) {
+  return (
+    <SafeAreaView>
+      <Text>Blinds Information Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default BlindsInformationScreen;
