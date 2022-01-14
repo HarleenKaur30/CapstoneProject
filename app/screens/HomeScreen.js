e6a0d2fe9b78961c92c4b6034d5dc309bd45d924
Harleen <43519990+HarleenKaur30@users.noreply.github.com>
2022-01-13 20:34:12 -0700

    Update HomeScreen.js

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,35 +1,58 @@
 import React from "react";
 import {
-  Text,
-  View,
   SafeAreaView,
-  Platform,
   StyleSheet,
-  TouchableOpacity,
+  Text,
+  ScrollView,
+  FlatList,
+  View,
   Image,
+  TextInput,
 } from "react-native";
+import { useState, useEffect } from "react";
 import colors from "../../app/config/colors";
 import values from "../../app/config/values";
 import Button from "../components/Button";
 import { MaterialIcons } from "@expo/vector-icons";
+import { Card } from "react-native-elements";
 
-function HomeScreen({ navigation }) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <MaterialIcons
-        style={styles.icon}
-        name="library-add"
-        size={60}
-        color="lightgray"
-      />
+const HomeScreen = ({ navigation }) => {
+  const [houseCount, setHouseCount] = useState(values.houses.number);
 
-      <Button
-        title="Add Device"
-        onPress={() => navigation.navigate("Add Device")}
-      />
-    </SafeAreaView>
-  );
-}
+  if (!houseCount) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <MaterialIcons
+          style={styles.icon}
+          name="library-add"
+          size={60}
+          color="lightgray"
+        />
+
+        <Button
+          title="Add Device"
+          onPress={() => navigation.navigate("Add Device")}
+        />
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <MaterialIcons
+          style={styles.icon}
+          name="library-add"
+          size={60}
+          color="lightgray"
+        />
+
+        <Button
+          title="Add Device"
+          onPress={() => navigation.navigate("Add Device")}
+        />
+      </SafeAreaView>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -43,6 +66,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 15,
+  },
+
+  scrolling: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
 });
 
