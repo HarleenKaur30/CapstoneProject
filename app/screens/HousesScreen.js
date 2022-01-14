6edb4521765e7128ae6a1699442297bdf81c5743
gjsingh10 <gjsingh@ualberta.ca>
2022-01-14 00:58:24 -0700

    Update HousesScreen.js

--- app/screens/HousesScreen.js
+++ app/screens/HousesScreen.js
@@ -1,35 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, FlatList, View, Alert } from "react-native";
+import { Button } from "react-native-elements/dist/buttons/Button";
 
 import ListHouse from "../components/ListHouse";
 import ListItemSeperator from "../components/ListItemSeperator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
+import ListItemEditAction from "../components/ListItemEditAction";
 import colors from "../config/colors";
-import values from "../config/values";
 import houses from "../config/houses";
+import values from "../config/values";
 
 function HousesScreen({ navigation }) {
+  const [newHouses, setNewHouses] = useState(houses);
+
+  const handleDelete = (message) => {
+    setNewHouses(newHouses.filter((m) => m.id !== message.id));
+  };
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={houses}
-        keyExtractor={(message) => message.id.toString()}
-        renderItem={({ item }) => (
-          <ListHouse
-            houseName={item.name}
-            numberBlinds={item.numberBlinds}
-            onPress={() => navigation.navigate("Blinds", { house: item.title })}
+    <View style={styles.largeContainer}>
+      <View style={styles.container}>
+        <FlatList
+          data={newHouses}
+          keyExtractor={(message) => message.id.toString()}
+          renderItem={({ item }) => (
+            <ListHouse
+              houseName={item.name}
+              numberBlinds={item.numberBlinds}
+              onPress={() =>
+                navigation.navigate("Blinds", { house: item.title })
+              }
+              onLongPress={() =>
+                Alert.alert(
+                  "House Menu",
+                  "What would you like to do with this house?",
+                  [
+                    { text: "Cancel" },
+                    {
+                      text: "Edit",
+                      onPress: () =>
+                        navigation.navigate("Add House", { house: item.title }),
+                    },
+                    {
+                      text: "Delete",
+                      onPress: () =>
+                        Alert.alert(
+                          "Delete",
+                          "Are you sure you would like to delete this house?",
+                          [
+                            { text: "Cancel" },
+                            { text: "Yes", onPress: () => handleDelete(item) },
+                          ]
+                        ),
+                    },
+                  ]
+                )
+              }
+              renderRightActions={() => (
+                <ListItemDeleteAction
+                  onPress={() =>
+                    Alert.alert(
+                      "Delete",
+                      "Are you sure you would like to delete this house?",
+                      [
+                        { text: "Cancel" },
+                        { text: "Yes", onPress: () => handleDelete(item) },
+                      ]
+                    )
+                  }
+                />
+              )}
+              renderLeftActions={() => (
+                <ListItemEditAction
+                  onPress={() =>
+                    navigation.navigate("Add House", { house: item.title })
+                  }
+                />
+              )}
+            />
+          )}
+          ItemSeparatorComponent={ListItemSeperator}
+        />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Add House"
+            onPress={() => {
+              values.maxNumberOfHouses === values.houses.number
+                ? Alert.alert(
+                    "House Could Not Be Added",
+                    "The maximum number of houses have already been added using this device.",
+                    [{ text: "Ok" }]
+                  )
+                : navigation.navigate("Add House");
+            }}
           />
-        )}
-        ItemSeparatorComponent={ListItemSeperator}
-      />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: colors.orange,
+    borderRadius: 25,
+    width: "40%",
+    marginHorizontal: "30%",
+    marginVertical: "5%",
+  },
   container: {
-    flex: 1,
     backgroundColor: colors.white,
+  },
+  largeContainer: {
+    backgroundColor: colors.white,
+    flex: 1,
   },
 });
 
