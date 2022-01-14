7610bfee734546f6a7fe3532cfad4b2fefcda1ee
gjsingh10 <gjsingh@ualberta.ca>
2022-01-13 20:22:08 -0700

    Create HousesScreen.js

---
+++ app/screens/HousesScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+
+function HousesScreen(props) {
+  return (
+    <SafeAreaView>
+      <Text>Houses</Text>
+    </SafeAreaView>
+  );
+}
+
+export default HousesScreen;
