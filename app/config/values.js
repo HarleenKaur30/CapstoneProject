331b412c6b8f20f17aff4cabfe24f8d7e337f667
Harleen <43519990+HarleenKaur30@users.noreply.github.com>
2022-01-16 14:08:52 -0700

    Update values.js

--- app/config/values.js
+++ app/config/values.js
@@ -1,25 +1,37 @@
 export default {
   maxNumberOfHouses: 5,
   maxNumberOfBlinds: 10,
+
   houses: {
     number: 3,
+
     house1: {
       id: 1,
       name: "House",
-      blinds: 7,
+
+      blinds: 2,
+
       blind1: {
-        name: "Bedroom",
+        name: "Bedroom 1",
+        group: "Bedroom",
+        openPercentage: 36,
+      },
+      blind2: {
+        name: "Bedroom 2",
+        group: "Bedroom",
+        openPercentage: 82,
       },
     },
+
     house2: {
       id: 2,
       name: "Vacation House",
-      blinds: 9,
+      blinds: 2,
     },
     house3: {
       id: 3,
       name: "Work",
-      blinds: 10,
+      blinds: 1,
     },
     house4: {
       id: 4,
