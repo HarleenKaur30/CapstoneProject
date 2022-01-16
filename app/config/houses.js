ba966da7cbb39e45dfc5dde4fd0475240ad41abc
Harleen <43519990+HarleenKaur30@users.noreply.github.com>
2022-01-16 14:47:06 -0700

    Update houses.js

--- app/config/houses.js
+++ app/config/houses.js
@@ -7,40 +7,27 @@ export default [
     name: values.houses.house1.name,
     numberBlinds: values.houses.house1.blinds,
 
-    blind1: {
-      name: "Bedroom 1",
-      group: "Bedroom",
-      openPercentage: 36,
-    },
-
-    blind2: { name: "Bedroom 2", group: "Bedroom", openPercentage: 82 },
+    blinds: [
+      { id: 1, name: "Bedroom 1", group: "Bedroom", openPercentage: 36 },
+      { id: 2, name: "Bedroom 2", group: "Bedroom", openPercentage: 82 },
+    ],
   },
 
-  /* {
+  {
     title: "house2",
     id: values.houses.house2.id,
     name: values.houses.house2.name,
     numberBlinds: values.houses.house2.blinds,
-    blind1: {
-      name: "Kitchen",
-      group: "Kitchen",
-      openPercentage: 0,
-    },
-    blind2: {
-      name: "Bedroom 5",
-      group: "Bedroom",
-      openPercentage: 100,
-    },
+    blinds: [
+      { id: 1, name: "Hallway 2", group: "Hallway", openPercentage: 0 },
+      { id: 2, name: "Bedroom Blind", group: "Bedroom", openPercentage: 100 },
+    ],
   },
   {
     title: "house3",
     id: values.houses.house3.id,
     name: values.houses.house3.name,
     numberBlinds: values.houses.house3.blinds,
-    blind1: {
-      name: "Kitchen 2",
-      group: "Kitchen",
-      openPercentage: 50,
-    },
-  }, */
+    blinds: [{ id: 1, name: "Kitchen", group: "Kitchen", openPercentage: 55 }],
+  },
 ];
