d053f10a2c97bfb9d32ece3c14462856735ead00
Harleen <43519990+HarleenKaur30@users.noreply.github.com>
2022-01-16 14:08:48 -0700

    Create ListBlind.js

---
+++ app/components/ListBlind.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { View, StyleSheet, Text, TouchableHighlight } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import colors from "../config/colors";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+function ListBlind({
+  blindName,
+  openPercetage,
+  blindGroup,
+  onPress,
+  onLongPress,
+  renderRightActions,
+  renderLeftActions,
+}) {
+  return (
+    <GestureHandlerRootView>
+      <Swipeable
+        renderRightActions={renderRightActions}
+        renderLeftActions={renderLeftActions}
+      >
+        <TouchableHighlight
+          onPress={onPress}
+          onLongPress={onLongPress}
+          underlayColor={colors.silvergray}
+        >
+          <View style={styles.container}>
+            <MaterialCommunityIcons
+              name="blinds"
+              size={40}
+              color={colors.orange}
+            />
+            <View style={styles.textContainer}>
+              <Text style={styles.blindNameText}>{blindName}</Text>
+              <Text style={styles.blindsInfo}>
+                {openPercetage}% open, Group: {blindGroup}
+              </Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Swipeable>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  blindsInfo: {
+    color: colors.medium,
+    fontSize: 13,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    fontStyle: "italic",
+  },
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    padding: "5%",
+    backgroundColor: colors.white,
+  },
+  blindNameText: {
+    color: colors.dark,
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    fontWeight: "bold",
+  },
+  textContainer: {
+    paddingLeft: "5%",
+  },
+});
+
+export default ListBlind;
