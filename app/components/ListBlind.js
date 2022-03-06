8c0bcabc12426e82093408f0bbb56add8ad3077f
Harleen <43519990+HarleenKaur30@users.noreply.github.com>
2022-03-06 14:58:51 -0700

    Revert "Update ListBlind.js"
    This reverts commit 34b09b9b47055f9bf31683824b6231cfa00e7f14.

--- app/components/ListBlind.js
+++ app/components/ListBlind.js
@@ -29,7 +29,7 @@ function ListBlind({
             <MaterialCommunityIcons
               name="blinds"
               size={40}
-              color={colors.logo_blue}
+              color={colors.orange}
             />
             <View style={styles.textContainer}>
               <Text style={styles.blindNameText}>{blindName}</Text>
