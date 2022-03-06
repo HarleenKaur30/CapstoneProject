8f20d151840cf80b6d63f8d47b5f488fc58e7803
Harleen <43519990+HarleenKaur30@users.noreply.github.com>
2022-03-06 14:48:49 -0700

    Update ListBlindSchedule.js

--- app/components/ListBlindSchedule.js
+++ app/components/ListBlindSchedule.js
@@ -25,7 +25,7 @@ function ListBlindSchedule({
             <MaterialCommunityIcons
               name="blinds"
               size={40}
-              color={colors.orange}
+              color={colors.logo_blue}
             />
             <View style={styles.textContainer}>
               <Text style={styles.blindNameText}>{blindName}</Text>
