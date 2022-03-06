82e8eeb0f7695c5d4c4ba33ecf375d8098494fa9
Harleen <43519990+HarleenKaur30@users.noreply.github.com>
2022-03-06 14:58:58 -0700

    Revert "Update AppButton.js"
    This reverts commit e656053c5a1a035125aef0f8186daf820a59af16.

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import colors from "../config/colors";
 
-function AppButton({ title, onPress, color = "logo_blue" }) {
+function AppButton({ title, onPress, color = "orange" }) {
   return (
     <TouchableOpacity
       style={[styles.button, { backgroundColor: colors[color] }]}
@@ -15,12 +15,12 @@ function AppButton({ title, onPress, color = "logo_blue" }) {
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: colors.logo_blue,
+    backgroundColor: colors.orange,
     borderRadius: 25,
     justifyContent: "center",
     alignItems: "center",
     padding: 10,
-    width: "50%",
+    width: "40%",
     marginVertical: 10,
   },
   text: {
