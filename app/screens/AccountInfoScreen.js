7285f9806b2d86bef45dade5202d560082d77519
gjsingh10 <gjsingh@ualberta.ca>
2022-03-24 23:27:29 -0600

    Update PasswordUpdate
    Issue has been figured out and fixed. Works as expected, now.

--- app/screens/AccountInfoScreen.js
+++ app/screens/AccountInfoScreen.js
@@ -7,40 +7,101 @@ import {
   Modal,
   Pressable,
   TextInput,
+  Alert,
+  TouchableOpacity,
 } from "react-native";
 import colors from "../config/colors";
-
+import ip from "../config/ip";
+import AppTextInput from "../components/AppTextInput";
+//import { useDerivedValue } from "react-native-reanimated";
+import AppButton from "../components/AppButton";
 
 function AccountScreen({ navigation }) {
-    const [userEmail, setuserEmail]=React.useState('johndoe@mailservice.com'); // import these values either via API call to OAUTH or through a data base query
-    const [userPassword, setuserPassword]=React.useState('123456');
-    const [buffer, setbuffer]=React.useState(null);
+  const [email, setEmail] = React.useState(global.email); // import these values either via API call to OAUTH or through a data base query
+  const [currentPassword, setCurrentPassword] = React.useState(global.password);
+  const [buffer, setbuffer] = React.useState(null);
+  const [newPassword, setnewPassword] = useState();
 
-    const [emailModalVisible, setemailModalVisible] = useState(false);
-    const [passwordModalVisible, setpasswordModalVisible] = useState(false);
+  const [emailModalVisible, setemailModalVisible] = useState(false);
+  const [passwordModalVisible, setpasswordModalVisible] = useState(false);
 
-    return (
-        <View style={styles.parentContainer}>
-            <View style={styles.list}>
-                <Text style={styles.textStyle}> Password: <Text style={styles.infoStyle}>{userPassword}</Text></Text>
-                <Pressable
-                style={[styles.resetButton]}
-                onPress={() => setpasswordModalVisible(true)}
-                >
-                    <Text style={styles.infoStyle}>Update</Text>
-                </Pressable>
-            </View>
-            <View style={styles.list}>
-                <Text style={styles.textStyle}> Email: <Text style={styles.infoStyle}>{userEmail}</Text></Text>
-                <Pressable
+  ChangeRecord = () => {
+    newPassword;
+
+    if (newPassword.length == 0) {
+      Alert.alert(
+        "Password could not be updated.",
+        "New password cannot be blank."[{ text: "Ok" }]
+      );
+    } else {
+      {
+        var SearchAPIURL =
+          "http://" + ip.ip + ":" + ip.port + "/api/password_reset.php";
+
+        var headers = {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        };
+
+        var data = {
+          userID: global.userID,
+          newPassword: newPassword,
+        };
+
+        fetch(SearchAPIURL, {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(data),
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            Alert.alert("Password Updated.", response[0].Message, [
+              {
+                text: "Ok",
+                onPress: () => navigation.navigate("User Info"),
+              },
+            ]);
+            global.password = newPassword;
+          })
+          .catch((error) => {
+            Alert.alert(
+              "Password could not be updated.",
+              "Error Insert: " + error,
+              [{ text: "Ok" }]
+            );
+          });
+      }
+    }
+  };
+
+  return (
+    <View style={styles.parentContainer}>
+      <View style={styles.list}>
+        <Text style={styles.textStyle}>
+          {" "}
+          Password: <Text style={styles.infoStyle}>{currentPassword}</Text>
+        </Text>
+        <Pressable
+          style={[styles.resetButton]}
+          onPress={() => setpasswordModalVisible(true)}
+        >
+          <Text style={styles.infoStyle}>Update</Text>
+        </Pressable>
+      </View>
+      <View style={styles.list}>
+        <Text style={styles.textStyle}>
+          {" "}
+          Email: <Text style={styles.infoStyle}>{email}</Text>
+        </Text>
+        {/* <Pressable
                 style={[styles.resetButton]}
                 onPress={() => setemailModalVisible(true)}
                 >
                     <Text style={styles.infoStyle}>Update</Text>
-                </Pressable>
-            </View>
+                </Pressable> */}
+      </View>
 
-            <Modal
+      {/* <Modal
             animationType="slide"
             transparent={true}
             visible={emailModalVisible}
@@ -63,7 +124,7 @@ function AccountScreen({ navigation }) {
                         />
                         <Pressable
                             style={styles.OKButton}
-                            onPress={() => {setemailModalVisible(!emailModalVisible), setuserEmail(buffer)}}
+                            onPress={() => {setemailModalVisible(!emailModalVisible), setEmail(buffer)}}
                         >
                             <Text style={styles.controlButtonText}>OK</Text>
                         </Pressable>
@@ -75,58 +136,58 @@ function AccountScreen({ navigation }) {
                         </Pressable>
                     </View>
                 </View>
-            </Modal>
+            </Modal> */}
 
-            <Modal
-            animationType="slide"
-            transparent={true}
-            visible={passwordModalVisible}
-            onRequestClose={() => {
-                setpasswordModalVisible(!passwordModalVisible);
-            }}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={passwordModalVisible}
+        onRequestClose={() => {
+          setpasswordModalVisible(!passwordModalVisible);
+        }}
+      >
+        <View style={styles.popupParentContainer}>
+          <View style={styles.popupContainer}>
+            <View style={styles.instructionContainer}>
+              <Text style={styles.textStyle}>Enter new password</Text>
+            </View>
+            <TextInput
+              style={styles.input}
+              placeholder="  new password"
+              onChangeText={(text) => setnewPassword(text)}
+            />
+
+            <Pressable
+              style={styles.OKButton}
+              onPress={() => {
+                setpasswordModalVisible(!passwordModalVisible),
+                  setCurrentPassword(newPassword),
+                  ChangeRecord(newPassword);
+              }}
             >
-                <View style={styles.popupParentContainer}>
-                    <View style={styles.popupContainer}>
-                        <View style={styles.instructionContainer}>
-                            <Text style={styles.textStyle}>
-                                Enter new password
-                            </Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="  new password"
-                            onChangeText={setbuffer}
-                        />
-
-                        <Pressable
-                            style={styles.OKButton}
-                            onPress={() => {setpasswordModalVisible(!passwordModalVisible), setuserPassword(buffer)}}
-                        >
-                            <Text style={styles.controlButtonText}>OK</Text>
-                        </Pressable>
-                        <Pressable
-                            style={styles.cancelButton}
-                            onPress={() => setpasswordModalVisible(!passwordModalVisible)}
-                        >
-                            <Text style={styles.controlButtonText}>CANCEL</Text>
-                        </Pressable>
-                    </View>
-                </View>
-            </Modal>
-
-
+              <Text style={styles.controlButtonText}>OK</Text>
+            </Pressable>
+            <Pressable
+              style={styles.cancelButton}
+              onPress={() => setpasswordModalVisible(!passwordModalVisible)}
+            >
+              <Text style={styles.controlButtonText}>CANCEL</Text>
+            </Pressable>
+          </View>
         </View>
-    );
+      </Modal>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    parentContainer: {
+  parentContainer: {
     height: "100%",
     backgroundColor: colors.white,
     padding: "2%",
     alignItems: "center",
-    },
-    list: {
+  },
+  list: {
     height: 60,
     width: "100%",
     padding: "5%",
@@ -134,20 +195,20 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     borderBottomColor: colors.light,
     borderBottomWidth: 1,
-    },
-    textStyle: {
+  },
+  textStyle: {
     color: colors.black,
     fontSize: 16,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
     fontWeight: "bold",
-    },
-    infoStyle: {
+  },
+  infoStyle: {
     color: colors.black,
     fontSize: 16,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
     fontWeight: "normal",
-    },
-    resetButton: {
+  },
+  resetButton: {
     height: 30,
     width: "25%",
     backgroundColor: colors.white,
@@ -159,46 +220,45 @@ const styles = StyleSheet.create({
     marginTop: "2%",
     position: "absolute",
     right: "5%",
-    },
-    popupParentContainer: {
+  },
+  popupParentContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    },
-    popupContainer: {
+  },
+  popupContainer: {
     width: "90%",
     height: 150,
     backgroundColor: colors.white,
     borderRadius: 20,
     shadowColor: "#000",
     shadowOffset: {
-        width: 0,
-        height: 2
+      width: 0,
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
-    },
-    OKButton: {
+    elevation: 5,
+  },
+  OKButton: {
     backgroundColor: colors.white,
     position: "absolute",
     bottom: "10%",
     right: "15%",
-    },
-    cancelButton: {
+  },
+  cancelButton: {
     backgroundColor: colors.white,
     position: "absolute",
     bottom: "10%",
     right: "35%",
-    },
-    controlButtonText: {
+  },
+  controlButtonText: {
     color: "blue",
     fontSize: 15,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
     fontWeight: "normal",
-
-    },
-    input: {
+  },
+  input: {
     height: "30%",
     width: "90%",
     borderWidth: 1,
@@ -206,16 +266,16 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: "30%",
     right: "5%",
-    },
-    instructionContainer: {
-        height: "15%",
-        width: "90%",
-        position: "absolute",
-        top: "10%",
-        right: "5%",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  },
+  instructionContainer: {
+    height: "15%",
+    width: "90%",
+    position: "absolute",
+    top: "10%",
+    right: "5%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default AccountScreen;
--- app/screens/OptimizedScheduleScreenWithEnergy.js
+++ app/screens/OptimizedScheduleScreenWithEnergy.js
@@ -1,130 +1,189 @@
 // screen to be used when energy savings data is available. Includes additional info bar showing evergy savings and button
-// to navigate to EnergySavingsScreen. 
+// to navigate to EnergySavingsScreen.
 //To use this screen replace "import OptimizedScheduleScreen from "./app/screens/OptimizedScheduleScreen";" in App.js with "import OptimizedScheduleScreen from "./app/screens/OptimizedScheduleScreenWithEnergy";"
 
-import React, {Component} from 'react';
-import { Text, View, SafeAreaView, Platform, StyleSheet, TouchableOpacity, RefreshControl, Alert} from 'react-native';
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  RefreshControl,
+  Alert,
+} from "react-native";
 import colors from "../config/colors";
-import { MaterialCommunityIcons, FontAwesome, Feather, FontAwesome5 } from "@expo/vector-icons";
-import Timeline from 'react-native-timeline-flatlist'
+import {
+  MaterialCommunityIcons,
+  FontAwesome,
+  Feather,
+  FontAwesome5,
+} from "@expo/vector-icons";
+import Timeline from "react-native-timeline-flatlist";
 
-const energySavings=25.2 // need to import this value from backend energy savings calculation
-
+const energySavings = 25.2; // need to import this value from backend energy savings calculation
 
 export default class OptScheduleScreen extends Component {
-  constructor(props){
-    super(props)
-    this.data = [ // import this from local buffer, filled from API at midnight each day
-      {time: '3:00am', title: 'Minimum Outdoor Temperature', icon: <FontAwesome5 name="thermometer-quarter" size={30} color={colors.black}/>},
-      {time: '9:00am', title: 'Sunrise', icon: <Feather name="sunrise" size={30} color={colors.black}/>},
-      {time: '10:45am', title: 'Close Blinds', icon: <MaterialCommunityIcons name="blinds" size={30} color={colors.black}/>},
-      {time: '12:00pm', title: 'Peak Outdoor Temperature', icon: <FontAwesome5 name="thermometer-full" size={30} color={colors.black}/>},
-      {time: '4:00pm', title: 'Open Blinds', icon: <MaterialCommunityIcons name="blinds-open" size={30} color={colors.black}/>},
-      {time: '7:00pm', title: 'Sunset', icon: <Feather name="sunset" size={30} color={colors.black}/>},
-      {time: '12:00am', title: 'Next Forecast Update', icon: <FontAwesome name="refresh" size={30} color={colors.black}/>}
-    ]
+  constructor(props) {
+    super(props);
+    this.data = [
+      // import this from local buffer, filled from API at midnight each day
+      {
+        time: "3:00am",
+        title: "Minimum Outdoor Temperature",
+        icon: (
+          <FontAwesome5
+            name="thermometer-quarter"
+            size={30}
+            color={colors.black}
+          />
+        ),
+      },
+      {
+        time: "9:00am",
+        title: "Sunrise",
+        icon: <Feather name="sunrise" size={30} color={colors.black} />,
+      },
+      {
+        time: "10:45am",
+        title: "Close Blinds",
+        icon: (
+          <MaterialCommunityIcons
+            name="blinds"
+            size={30}
+            color={colors.black}
+          />
+        ),
+      },
+      {
+        time: "12:00pm",
+        title: "Peak Outdoor Temperature",
+        icon: (
+          <FontAwesome5
+            name="thermometer-full"
+            size={30}
+            color={colors.black}
+          />
+        ),
+      },
+      {
+        time: "4:00pm",
+        title: "Open Blinds",
+        icon: (
+          <MaterialCommunityIcons
+            name="blinds-open"
+            size={30}
+            color={colors.black}
+          />
+        ),
+      },
+      {
+        time: "7:00pm",
+        title: "Sunset",
+        icon: <Feather name="sunset" size={30} color={colors.black} />,
+      },
+      {
+        time: "12:00am",
+        title: "Next Forecast Update",
+        icon: <FontAwesome name="refresh" size={30} color={colors.black} />,
+      },
+    ];
     this.state = {
       refreshing: false,
     };
   }
 
   _onRefresh = () => {
-    this.setState({refreshing: true});
+    this.setState({ refreshing: true });
     setTimeout(() => {
       this.setState({
         data: this.data, // add API call to update this.data
-        refreshing: false
+        refreshing: false,
       });
     }, 2000);
-  }
+  };
 
   render() {
     return (
-        <SafeAreaView style={styles.container}>
-
-          <View style={styles.houseContainer}> 
-          {/* this text must be a dynamic variable set equal to the house selected from the Automation screen picker */}
-          <Text style={styles.timelineText}> 
-              House 1  
+      <SafeAreaView style={styles.container}>
+        {/* <View style={styles.houseContainer}> 
+            <Text style={styles.timelineText}> 
+                House 1  
             </Text>        
-          </View>
+          </View> */}
 
-          <TouchableOpacity style={styles.button}
-            onPress={() => {
-              // add backend connection to apply schedule
-              Alert.alert(
-                "Optimizer",
-                "Schedule Applied",
-                [{ text: "Ok" }]
-              )
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            // add backend connection to apply schedule
+            Alert.alert("Optimizer", "Schedule Applied", [{ text: "Ok" }]);
+          }}
+        >
+          <Text style={styles.buttonText}>Apply Optimized Schedule</Text>
+        </TouchableOpacity>
+
+        <View style={styles.timelineContainer} nestedScrollEnabled={true}>
+          <Timeline
+            style={styles.timeline}
+            data={this.data}
+            circleSize={40}
+            circleColor={colors.white}
+            lineColor={colors.orange}
+            timeContainerStyle={{ minWidth: 72, marginTop: 0 }}
+            timeStyle={{
+              textAlign: "center",
+              backgroundColor: colors.light,
+              color: colors.black,
+              padding: 5,
+              borderRadius: 4,
+              borderWidth: 2,
+              borderColor: colors.logo_blue,
+              marginTop: "10%",
             }}
-          >
-            <Text style={styles.buttonText}>
-              Apply Optimized Schedule
-            </Text>
-          </TouchableOpacity>
+            descriptionStyle={{ color: "gray" }}
+            options={{
+              style: { paddingTop: 5 },
+              refreshControl: (
+                <RefreshControl
+                  refreshing={this.state.refreshing}
+                  onRefresh={this._onRefresh}
+                />
+              ),
+            }}
+            titleStyle={{ marginBottom: "10%" }}
+            innerCircle={"icon"}
+            separator={true}
+          />
+        </View>
 
-          <View style={styles.timelineContainer} nestedScrollEnabled={true}>
-            <Timeline 
-              style={styles.timeline}
-              data={this.data}
-              circleSize={40}
-              circleColor={colors.white}
-              lineColor={colors.orange}
-              timeContainerStyle={{minWidth:52, marginTop: 0}}
-              timeStyle={{
-                textAlign: 'center', 
-                backgroundColor: colors.light, 
-                color: colors.black, 
-                padding:5, 
-                borderRadius:4, 
-                borderWidth:2, 
-                borderColor: colors.logo_blue, 
-                marginTop: "10%"}}
-              descriptionStyle={{color:'gray'}}
-              options={{
-                style:{paddingTop:5},
-                refreshControl: (
-                  <RefreshControl
-                    refreshing={this.state.refreshing}
-                    onRefresh={this._onRefresh}
-                  />
-                ),}}
-              titleStyle={{marginBottom: "10%"}}
-              innerCircle={'icon'}
-              separator={true}
-              timeContainerStyle={{minWidth:72}}
-            />
-          </View>
-
-          <View style={styles.energySavingsContainer}>
-            <Text style={{color: 
-              colors.black, 
-              fontSize: 14, 
-              fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir", 
+        <View style={styles.energySavingsContainer}>
+          <Text
+            style={{
+              color: colors.black,
+              fontSize: 14,
+              fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
               fontWeight: "bold",
               paddingLeft: "2%",
-            }}>
-              Estimated Energy Savings: <Text style={styles.energyText}>{energySavings} kWh</Text>
-            </Text>
+            }}
+          >
+            Estimated Energy Savings:{" "}
+            <Text style={styles.energyText}>{energySavings} kWh</Text>
+          </Text>
 
-            <TouchableOpacity style={styles.smallButton}
-                onPress={() => {
-                  this.props.navigation.navigate("Energy Savings");
-              }}
-            >
-              <Text style={styles.smallText}>
-                View Details
-              </Text>
-            </TouchableOpacity>
-          </View>
-
-        </SafeAreaView>
+          <TouchableOpacity
+            style={styles.smallButton}
+            onPress={() => {
+              this.props.navigation.navigate("Energy Savings");
+            }}
+          >
+            <Text style={styles.smallText}>View Details</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     );
   }
-
 }
-
 
 const styles = StyleSheet.create({
   buttonText: {
@@ -217,6 +276,6 @@ const styles = StyleSheet.create({
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
     fontWeight: "bold",
   },
-}); 
+});
 
 export const OptimizedScheduleScreen = OptScheduleScreen;
